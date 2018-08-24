6fbb010bc02aa064907fd5ec1b9e30bbbac4295f
Eduardo A. Cruz Junior <eduardo_acj2@hotmail.com>
2018-08-24 02:42:54 -0300

    inicio da implementacao do metodo de inversao

---
+++ src/metodos/InversaoAproximada.java
@@ -0,0 +1,20 @@
+
+package metodos;
+
+public class InversaoAproximada {
+    
+    public static double[] inverter(double[] entrada){
+        
+        //Problemas no algoritmo
+        //os valores estao saindo negativos
+        for (int i = 0; i < entrada.length; i++) {
+            
+            entrada[i] = (Math.pow(entrada[i], 0.135) - Math.pow(1-entrada[i], 0.135)) / 0.1975;
+            
+        }
+        
+        return entrada;
+            
+    }
+    
+}
