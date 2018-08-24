57b8b96c2fddace0675965f7e9e5ac34369ee3ad
Eduardo A. Cruz Junior <eduardo_acj2@hotmail.com>
2018-08-24 02:24:05 -0300

    implementacao metodo de geracao

--- src/metodos/RandomCongruenteLinear.java
+++ src/metodos/RandomCongruenteLinear.java
@@ -3,4 +3,22 @@ package metodos;
 
 public class RandomCongruenteLinear {
     
+    public static int m = 2147483647;
+    public static int a = 25717;
+    
+    public static double[] gerarArray(int n){
+        
+        double z;
+        double za = 1;
+        double[] x = new double[n];
+        for (int i = 0; i < n; i++) {
+            z = (a*za)%m;
+            x[i] = (double) za/(m-1);
+            za = z;
+        }
+        
+        return x;
+        
+    }
+    
 }
