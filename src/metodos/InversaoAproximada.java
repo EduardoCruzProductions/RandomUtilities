80fc1487f5f218becb9cef96127c4af03abdfa26
Eduardo A. Cruz Junior <eduardo_acj2@hotmail.com>
2018-08-29 15:27:43 -0300

    removendo comentarios

--- src/metodos/InversaoAproximada.java
+++ src/metodos/InversaoAproximada.java
@@ -4,9 +4,7 @@ package metodos;
 public class InversaoAproximada {
     
     public double[] inverter(double[] entrada){
-        
-        //Problemas no algoritmo
-        //os valores estao saindo negativos
+
         for (int i = 0; i < entrada.length; i++) {
             
             entrada[i] = (Math.pow(entrada[i], 0.135) - Math.pow(1-entrada[i], 0.135)) / 0.1975;
